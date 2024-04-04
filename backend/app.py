6a8a22c639264b5eaba2472596de54c0f7057c77
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-04-04 11:17:17 +0530

    added apis

--- backend/app.py
+++ backend/app.py
@@ -7,22 +7,96 @@ app = Flask(__name__)
 
 fmp_url = "https://financialmodelingprep.com/api"
 fmp_key = os.environ.get("FMP_KEY")
+free_fmp_key = os.environ.get("FREE_FMP_KEY")
 
-@app.route("/market_broadview/", methods=["GET"])
+
+def get_response(url):
+    response = requests.get(url)
+    return response.json()
+
+@app.route("/market-broadview/", methods=["GET"])
 def get_market_broadview():
     constituent = request.json.get('constituent')
     historical = request.json.get('historical')
-    print('ksksk')
     hist = ''
     if historical:
         hist = 'historical/'
-        
+
     url = f'{fmp_url}/{hist}v3/{constituent}_constituent?apikey={fmp_key}'
-    response = requests.get(url)
-    response = response.json()
+    response = get_response(url)
+
+    return jsonify({'success': True, 'data': response})
+
+@app.route("/calendar/", methods=["GET"])
+def get_calendar():
+    start_date = request.json.get('start_date') # 2021-11-10
+    end_date = request.json.get('end_date') # 2022-02-01
+    cal_url = request.json.get('cal_url')
+    ticker = request.json.get('ticker') or ''
+    key = free_fmp_key
+    version = 'v3'
+
+    if cal_url == 'earning-calendar-confirmed' or cal_url == 'ipo-calendar-confirmed':
+        version = 'v4'
+        key = fmp_key
+
+    duration = ''
+    if cal_url != 'earning_calendar':
+        duration = f'from={start_date}&to={end_date}'
+
+    # urls = [
+    #     '/earning_calendar/' # free,
+    #     '/historical/earning_calendar/',
+    #     '/api/v4/?&apikey=',
+    #     '/stock_dividend_calendar=' # free,
+    #     '/stock_split_calendar=' # free,
+    #     '/api/v4/?&apikey=',
+    # ]
+
+    url = f'{fmp_url}/api/{version}/{cal_url}{ticker}?{duration}&apikey={key}'
+
+    response = get_response(url)
+
     print('response', response)
     return jsonify({'success': True, 'data': response})
-    
-    
+
+@app.route("/market-cap/", methods=["GET"])
+def get_market_cap():
+    historical = request.json.get("historical") # bool
+    ticker = request.json.get('ticker') # AAPL
+    limit_val = request.json.get('limit_val') # 100
+    start = request.json.get("start") # 2023-10-10
+    end = request.json.get("end") # 2023-10-10
+
+    cap_url = 'market-capitalization'
+    limit = duration = ''
+    if historical:
+        cap_url = 'historical-market-capitalization'
+        limit = f'limit={limit_val}&'
+        duration = f'from={start}&to={end}'
+
+    url = f'{fmp_url}/api/v3/{cap_url}/{ticker}?{limit}{duration}&apikey={free_fmp_key}'
+
+    response = get_response(url)
+
+    print('response', response)
+    return jsonify({'success': True, 'data': response})
+
+@app.route("/analyst-estimate/", methods=["GET"])
+def get_analyst_estimate():
+    ticker = request.json.get('ticker')
+    recommendation = request.json.get('recommendation')
+
+    url = 'analyst-estimates'
+    if recommendation:
+        url = 'analyst-stock-recommendations'
+
+    url = f'{fmp_url}/api/v3/{url}/{ticker}?apikey={fmp_key}'
+    response = get_response(url)
+
+    print('response', response)
+    return jsonify({'success': True, 'data': response})
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
